8cd45f33cf995e7162438fcd9215b0ddf44fccfd
Ardelean Andrei <91498815+Ardelean-Claudiu-Andrei@users.noreply.github.com>
2025-04-14 17:30:00 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "Test@123", // ← parola ta reală de MySQL!
+  password: "Test@123", // parola MySQL
   database: "crud_app",
 });
 
